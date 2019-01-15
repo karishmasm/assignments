e6483b4d9b34a05d89d20ba7f11e35bbee315fc4
karishmasm <46235293+karishmasm@users.noreply.github.com>
2019-01-14 23:45:18 -0500

    "Debugged and made changes to the py calculator file"
    "Debugged and made changes to the py calculator file"

---
+++ calculator.py
@@ -0,0 +1,39 @@
+#This file will create a calculator and have four functions
+
+#function 1 = Multiply
+def multiply(a,b):
+	return 2 * 5
+
+
+#function 2 = divide
+def Divide (a,b):
+	return 10/5
+
+#function 3 = Add
+def Add (a,b):
+	return 2+5
+
+#function 4 = Subtract
+def Subtract (a,b):
+	return 2-5
+
+#function 5 = Square 
+def Square (a,b):
+	return 6**2
+
+#function 5 = Square 
+def cube (a,b):
+	return 6**3
+
+print("I'm going use the calculator functions to multiply 2 and 5")
+x = multiply (2,5)
+print(x)
+
+
+
+
+
+
+
+
+
