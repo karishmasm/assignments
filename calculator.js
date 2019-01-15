91446784ce4f9b427ab8cd948b151e8bb59aa492
karishmasm <46235293+karishmasm@users.noreply.github.com>
2019-01-14 23:43:07 -0500

    Debugged and made changes

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function multiply(a,b) { 
+    return 2 * 5
+}
+
+function subtract(a,b) { 
+    return 2 - 5
+}
+
+function divide(a,b) { 
+    return 10/5
+}
+
+function add(a,b) { 
+    return 2+5
+}
+
+console.log("I'm going use the calculator functions to multiply 5 and 6")
+var x = multiply(5,6)
+console.log(x)
